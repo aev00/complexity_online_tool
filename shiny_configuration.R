61b51c18cd8165e05cec8fd46e736adcba9f6977
Anna Valyogos <valyogos.anna@gmail.com>
2023-05-05 16:05:25 -0400

    Add configuration

---
+++ shiny_configuration.R
@@ -0,0 +1,7 @@
+### Shiny App configuration ###
+
+library("rsconnect")
+
+rsconnect::setAccountInfo(name='q35vkf-anna-valyogos', 
+                          token='809BD08AA0FCD8BC2D382D35DA77A609', 
+                          secret='16Qf7D1ARlem1LRKC+sdN+1yIIZF87ag86SZAmJR')
